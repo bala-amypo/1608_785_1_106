a04ed3db3a6e571b8822df3819ac41b00cfd925f
bala-amypo <bala@amypo.in>
2025-12-21 20:42:59 +0000

    User commit

--- src/main/java/com/example/demo/authcontroller/AuthController.java
+++ src/main/java/com/example/demo/authcontroller/AuthController.java
@@ -1,40 +1,40 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.AuthResponse;
-import com.example.demo.entity.User;
-import com.example.demo.service.UserService;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.dto.AuthRequest;
+// import com.example.demo.dto.AuthResponse;
+// import com.example.demo.entity.User;
+// import com.example.demo.service.UserService;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
 
-    private final UserService userService;
+//     private final UserService userService;
 
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
+//     public AuthController(UserService userService) {
+//         this.userService = userService;
+//     }
 
-    @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return userService.register(user);
-    }
+//     @PostMapping("/register")
+//     public User register(@RequestBody User user) {
+//         return userService.register(user);
+//     }
 
-    @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest request) {
+//     @PostMapping("/login")
+//     public AuthResponse login(@RequestBody AuthRequest request) {
 
-        User user = userService.findByEmail(request.getEmail());
+//         User user = userService.findByEmail(request.getEmail());
 
-        if (!user.getPassword().equals(request.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
-        }
+//         if (!user.getPassword().equals(request.getPassword())) {
+//             throw new RuntimeException("Invalid credentials");
+//         }
 
-        return new AuthResponse(
-                "DUMMY_TOKEN",
-                user.getId(),
-                user.getEmail(),
-                user.getRole()
-        );
-    }
-}
+//         return new AuthResponse(
+//                 "DUMMY_TOKEN",
+//                 user.getId(),
+//                 user.getEmail(),
+//                 user.getRole()
+//         );
+//     }
+// }
