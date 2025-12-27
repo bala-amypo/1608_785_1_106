aff30cb2380511ae669ad959f50d62b2245fa718
bala-amypo <bala@amypo.in>
2025-12-27 04:28:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.*;
+// import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.security.*;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class SwaggerConfig {
+
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Water Quality Compliance Monitoring API")
+//                         .description("Spring Boot REST API")
+//                         .version("1.0"))
+//                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+//                 .components(new Components()
+//                         .addSecuritySchemes("bearerAuth",
+//                                 new SecurityScheme()
+//                                         .name("bearerAuth")
+//                                         .type(SecurityScheme.Type.HTTP)
+//                                         .scheme("bearer")
+//                                         .bearerFormat("JWT")));
+//     }
+// }
